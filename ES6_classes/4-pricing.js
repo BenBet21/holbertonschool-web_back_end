3db13e47433a021c50cbac7d4543cabddb7e731a
BenBet21 <benoit.beti@gmail.com>
2024-07-18 16:36:11 +0200

    4. Pricing

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -30,7 +30,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency_name} (${this._currency_code})`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
