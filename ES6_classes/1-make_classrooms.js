1b85bf266216679bb865d9f8e0cf9408d4e23c79
BenBet21 <benoit.beti@gmail.com>
2024-07-18 11:54:27 +0200

    1. Let's make some classrooms

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
 import Classroom from './0-classroom';
 
 function initializeRooms() {
-  return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34)
-  ];
+  
+    const room1 = new Classroom(19);
+    const room2 = new Classroom(20);
+    const room3 = new Classroom(34);
+  
+    return [room1, room2, room3];
 }
