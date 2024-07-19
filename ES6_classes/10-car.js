2294b14b733294d069ca48d820c1c05e52e43891
BenBet21 <benoit.beti@gmail.com>
2024-07-19 12:15:44 +0200

    10. Vroom

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,18 +1,14 @@
 /* eslint-disable */
 export default class Car {
   constructor(brand, motor, color) {
-     if (typeof brand !== 'string')
-      throw new Error('brand must be a string');
-    if (typeof motor !== 'string')
-      throw new Error('motor must be a string');
-    if (typeof color !== 'string')
-      throw new Error('color must be a string');
+   
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    const clonedCar = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    return clonedCar;
   }
 }
