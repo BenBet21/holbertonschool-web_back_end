3ffd366b3096dc840c121f61819221dc9e69bb4d
BenBet21 <benoit.beti@gmail.com>
2024-07-19 09:16:30 +0200

    5. A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number')
+      throw new TypeError('Sqft must be a number');
+    this._sqft = sqft;
+    
+    if (new.target === Building) {
+      throw new Error('Building cannot be instantiated directly');
+    }
+
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+  set sqft(value) {
+    if (typeof value !== 'number')
+      throw new TypeError('Sqft must be a number');
+    this._sqft = value;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
