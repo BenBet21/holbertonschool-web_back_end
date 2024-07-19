c183114e43aa50cc731ea84a6f51b23ed539fce9
BenBet21 <benoit.beti@gmail.com>
2024-07-19 10:50:05 +0200

    8. Primitive - Holberton Class

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number')
+      throw new TypeError ('size must be a number')
+    if (typeof location !== 'string')
+      throw new TypeError !== ('location must be a string')
+    this._size = size;
+    this._code = code;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number')
+      throw new TypeError ('size must be a number');
+    this._name = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string')
+      throw new TypeError ('location must be a string');
+    this._location = value;
+  }
+
+  valueof() {
+    return this._size;
+  }
+
+  tostring() {
+    return this._location;
+  }
+
+
+
+
+
+
+}
