e359f5275ec5c9ef840589a0f5f960c614a8a108
BenBet21 <benoit.beti@gmail.com>
2024-07-22 17:47:41 +0200

    10. More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
-/* eslint-disable */
 export default function updateUniqueItems(map) {
- if (!map instanceof Map) 
-  throw new Error ('Cannot process');
- 
- map.forEach((value, key) => {
-  if (value === 1) {
-    map.set(key, 100);
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-});
 
-return map;
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
 }
