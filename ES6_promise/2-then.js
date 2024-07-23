7b02e5836927812e42a9c27495dd6140d51bd46e
BenBet21 <benoit.beti@gmail.com>
2024-07-23 12:55:37 +0200

    2. Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  return Promise((resolve, reject) => {
+  return promise
     .then(() => ({
       status: 200,
       body: 'success',
     }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
-  });
-}
+  }
