1fa4cdef3b7962a69537071897cb16667da2d8fa
BenBet21 <benoit.beti@gmail.com>
2024-07-24 15:28:50 +0200

    6. Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,8 +5,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then(results => results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
-  })));
+  .then(results => {
+    const promises = [];
+    results.forEach((promise) => {
+      promises.push({
+        status: promise.status,
+        value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+      });
+    });
+    return promises;
+  });
 }
