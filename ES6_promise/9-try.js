59b55e822654896d2f33c20b03df9a982b3f1b26
BenBet21 <benoit.beti@gmail.com>
2024-07-24 15:58:59 +0200

    9. Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
