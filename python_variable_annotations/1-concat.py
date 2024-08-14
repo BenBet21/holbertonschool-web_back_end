2e543d2be8ced9b8980eec615c3007f6251cf6c8
BenBet21 <benoit.beti@gmail.com>
2024-08-14 07:54:07 +0200

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Simple concat function with two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Concatenate
+        str1 & str2 and return
+    """
+    return str1 + str2
