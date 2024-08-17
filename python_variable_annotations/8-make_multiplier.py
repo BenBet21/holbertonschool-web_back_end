1d9f9fe2169e7c15915b17ea046571eb9e0193e0
BenBet21 <benoit.beti@gmail.com>
2024-08-17 07:39:45 +0200

    8. Complex types - functions

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -9,8 +9,8 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
         Return a function that multiplies
     """
     def multi(n: float) -> float:
-      """
-          Return the product of n and multiplier
-      """
-      return n * multiplier
+        """
+            Return the product of n and multiplier
+        """
+        return n * multiplier
     return multi
