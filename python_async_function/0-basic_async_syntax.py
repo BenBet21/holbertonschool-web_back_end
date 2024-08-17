ecf8394716b433b2545c92a91ac03f566f7e37f8
BenBet21 <benoit.beti@gmail.com>
2024-08-17 07:46:13 +0200

    0. The basics of async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Write an asynchronous coroutine named
 wait_random that takes an integer argument
@@ -5,7 +6,8 @@ max_delay (defaulting to 10). It should
 wait for a random delay between
 0 and max_delay seconds
 (inclusive, as a float) and then return it.
-Use the random module."""
+Use the random module.
+"""
 
 import asyncio
 import random
