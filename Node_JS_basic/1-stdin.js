91d8a0fb7c8822f4b26bfe7c6e275fe582ae121b
BenBet21 <benoit.beti@gmail.com>
2024-09-16 14:25:10 +0200

    1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,8 @@ console.log('Welcome to Holberton School, what is your name?')
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log('Your name is:', name);
-  console.log('This important software is now closing');
   process.exit();
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
